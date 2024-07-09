e9a1d148bdaa0bac67e083b6266881bbdde4c11e
Pavel Kuvshinov <pavelkuvsh1noff@mail.ru>
2024-07-09 21:37:54 +0300

    fix: fix some messages after errors.

--- my-react-study-app/src/4_widgets/errorSection/errorSection.tsx
+++ my-react-study-app/src/4_widgets/errorSection/errorSection.tsx
@@ -8,8 +8,15 @@ class ErrorSection extends Component {
                     <p className="main__item_text-bigger">Something went wrong.</p>
                     <p className="main__item_text-bigger">If you want, you can try to search something else...</p>
                     <p className="main__item_text-bigger">
-                        For searching first please type &quot;category&quot; in the search field and then type
-                        &quot;name&quot; of the item...
+                        For searching something first of all please type an empty request in the search field and sent
+                        it.
+                    </p>
+                    <p className="main__item_text-bigger">
+                        Then you can choose a category. For example: &quot;books&quot; or &quot;houses&quot;
+                    </p>
+                    <p className="main__item_text-bigger">
+                        Then you can choose an item of the category by typing name of the item. For example: &quot;A
+                        Game of Thrones&quot; or &quot;House Algood&quot;.
                     </p>
                 </div>
             </main>
