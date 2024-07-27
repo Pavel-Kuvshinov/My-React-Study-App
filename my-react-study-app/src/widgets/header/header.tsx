4341c7c8e72a7a1d0c45c26b678b074f7e44872d
Pavel Kuvshinov <pavelkuvsh1noff@mail.ru>
2024-07-27 17:37:46 +0300

    feat: Add switch theme button.

--- my-react-study-app/src/widgets/header/header.tsx
+++ my-react-study-app/src/widgets/header/header.tsx
@@ -1,9 +1,15 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import { useEffect, useState } from 'react';
 import './header.css';
 
 import SearchForm from '../searchForm/searchFrom';
 import { useAppDispatch } from '../../shared/store/store';
 import { itemsSlice } from '../../shared/store/itemsSlice';
+
+/* eslint-disable import/no-absolute-path */
+import brightness from '../../shared/assets/brightness-icon.svg';
+import moon from '../../shared/assets/moon-icon.svg';
+/* eslint-enable import/no-absolute-path */
 
 export default function Header() {
     const [newError, setNewError] = useState(false);
@@ -27,7 +33,9 @@ export default function Header() {
             </button>
             <SearchForm />
             <button type="button" className="theme__button">
-                theme
+                <svg className="theme__button-icon">
+                    <use xlinkHref={`${brightness}#brightness-icon`} />
+                </svg>
             </button>
         </header>
     );
